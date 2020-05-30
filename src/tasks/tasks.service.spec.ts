46fa07a1031e6cdf33bc588f3cc22e131873e1e0
Maruf Alom <maruf.alom@portonics.com>
2020-05-31 00:19:31 +0600

    test: getTasks() and getTasksById() test added

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,61 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './services/tasks.service';
+import { TaskRepository } from './repositories/task.repository';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './enum/task-status.enum';
+
+const mockUser = { id: 12, username: 'Test User' };
+
+const mockTaskRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+describe('TasksService', () => {
+  let tasksService;
+  let taskRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTaskRepository },
+      ],
+    }).compile();
+
+    tasksService = await module.get<TasksService>(TasksService);
+    taskRepository = await module.get<TaskRepository>(TaskRepository);
+  });
+
+  describe('getTasks', () => {
+    it('should gets all tasks from the repository', async () => {
+      taskRepository.getTasks.mockResolvedValue('someValue');
+      expect(taskRepository.getTasks).not.toHaveBeenCalled();
+
+      const filters: GetTasksFilterDto = {
+        status: TaskStatus.IN_PROGRESS,
+        search: 'Some search user',
+      };
+      const result = await tasksService.getTasks(filters, mockUser);
+      expect(taskRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTasksById', () => {
+    it('should calls taskRepository.findOne() and retrive and return the task', async () => {
+      const mockTask = {
+        title: 'test task',
+        description: 'Task desc',
+      };
+
+      taskRepository.findOne.mockResolvedValue(mockTask);
+
+      const result = await tasksService.getTaskById(1, mockUser);
+      expect(result).toEqual(mockTask);
+      expect(taskRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 1, userId: mockUser.id },
+      });
+    });
+  });
+});
